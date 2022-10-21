89a244c91851681e5748d67e962b164f82645ce3
Diego Novaes <dsnovaes1@gmail.com>
2022-10-20 22:11:45 -0700

    exercise 3 done

---
+++ W3D5/exercise3.rb
@@ -0,0 +1,24 @@
+class Map
+    def initialize
+        @my_map = []
+    end
+
+    def set(key,value)
+        @my_map.push([key,value]) unless get(key)
+    end
+
+    def get(key)
+        result = @my_map.select { |pair| pair.first == key }
+        result.empty? ? nil : result
+    end
+
+    def delete(key)
+        @my_map.each_with_index do |pair,idx|
+            @my_map.delete_at(idx) if pair.first == key
+        end
+    end
+
+    def show
+        @my_map
+    end
+end
